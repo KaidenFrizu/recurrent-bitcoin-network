78dabb4b615af1de02392733914ed1befd2cc57d
KaidenFrizu <39328974+KaidenFrizu@users.noreply.github.com>
2021-11-08 22:27:48 +0800

    Add Tensorflow model framework

---
+++ src/model.py
@@ -0,0 +1,23 @@
+from tensorflow.keras import Model
+
+from layers import Encoder
+from layers import Decoder
+
+class RBN(Model):
+    def __init__(self, t, k, H, units=32, **kwargs):
+        super(RBN, self).__init__(name='Recurrent Bitcoin Network', **kwargs)
+
+        self.encoder = Encoder(t=t, k=k, units=units)
+        self.decoder = Decoder(k=k, H=H, units=units)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x, initial_state):
+        return self.decoder(x, initial_state)
+
+    def call(self, x, training=True):
+        x = self.encode(x)
+        x = self.decode(x[0], x[1:])
+
+        return x
