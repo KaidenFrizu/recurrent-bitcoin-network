adeb28f7e617df3d3e2e7b51228c739b5098365c
KaidenFrizu <39328974+KaidenFrizu@users.noreply.github.com>
2021-11-14 13:50:07 +0800

    Revised function parameters

--- src/layers.py
+++ src/layers.py
@@ -2,34 +2,30 @@ from tensorflow.keras.layers import Layer
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import RepeatVector
 from tensorflow.keras.layers import TimeDistributed
 
 
 class Encoder(Layer):
-    def __init__(self, t, k, units, **kwargs):
+    def __init__(self, k, units, **kwargs):
         super(Encoder, self).__init__(name='Encoder', **kwargs)
-        self.input_layer = Input(shape=(t, k), dtype='float64')
-        self.lstm_encoder = LSTM(units=units, return_state=True)
+        self.input_layer = Input(shape=(None,k), dtype='float64')
+        self.lstm_encoder = LSTM(units=units, return_state=True, unroll=True)
 
     def call(self, x):
         x = self.input_layer(x)
-        x = self.lstm_encoder(x)
+        x, state_h, state_c = self.lstm_encoder(x)
 
-        return x
+        return x, state_h, state_c
 
 
 class Decoder(Layer):
-    def __init__(self, k, H, units, **kwargs):
+    def __init__(self, H, units, **kwargs):
         super(Decoder, self).__init__(name='Decoder', **kwargs)
-        self.repeat_vector = RepeatVector(H)
         self.lstm_decoder = LSTM(units=units, return_sequences=True)
-        self.resolve = Dense(k)
-        self.time_dist = TimeDistributed(self.resolve)
+        self.time_dist = TimeDistributed(Dense(H))
 
-    def call(self, x):
-        x = self.repeat_vector(x)
-        x = self.lstm_decoder(x)
+    def call(self, x, initial_state):
+        x = self.lstm_decoder(x, initial_state=initial_state)
         x = self.time_dist(x)
 
         return x
--- src/model.py
+++ src/model.py
@@ -1,22 +1,39 @@
 from tensorflow.keras import Model
 
+from typing import Optional
+
 from . import layers
 
 class RBN(Model):
-    def __init__(self, t, k, H, units=32, **kwargs):
+    def __init__(
+        self,
+        k: int,
+        H: int = 14,
+        units: int = 32,
+        encoder_kwargs: Optional[dict] = None,
+        decoder_kwargs: Optional[dict] = None,
+        **kwargs
+        ):
+
+        if encoder_kwargs is None:
+            encoder_kwargs = {}
+
+        if decoder_kwargs is None:
+            decoder_kwargs = {}
+
         super(RBN, self).__init__(name='Recurrent Bitcoin Network', **kwargs)
 
-        self.encoder = layers.Encoder(t=t, k=k, units=units)
-        self.decoder = layers.Decoder(k=k, H=H, units=units)
+        self.encoder = layers.Encoder(k=k, units=units, **encoder_kwargs)
+        self.decoder = layers.Decoder(k=k, H=H, units=units, **decoder_kwargs)
 
     def encode(self, x):
         return self.encoder(x)
 
-    def decode(self, x):
-        return self.decoder(x)
+    def decode(self, x, initial_state):
+        return self.decoder(x, initial_state)
 
     def call(self, x):
-        x = self.encode(x)
-        x = self.decode(x)
+        x, state_h, state_c = self.encode(x)
+        x = self.decode(x, initial_state=[state_h, state_c])
 
         return x
